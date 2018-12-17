866d57cc64fa6c81c69c1bcfce9d38e1e479d279
Dalton Vieira <daltonapolo11@gmail.com>
2018-12-17 03:33:01 -0200

    Començando tela de detalhe até a parte de Fragments

--- app/src/main/java/com/example/tecinfo/apk_final_tecinfo/Act_Detalhe_Estabelecimento.java
+++ app/src/main/java/com/example/tecinfo/apk_final_tecinfo/Act_Detalhe_Estabelecimento.java
@@ -1,21 +1,139 @@
 package com.example.tecinfo.apk_final_tecinfo;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Act_Detalhe_Estabelecimento extends AppCompatActivity {
+public class Act_Detalhe_Estabelecimento extends AppCompatActivity
+    implements Fragment_Ambiente.OnFragmentInteractionListener,
+        Fragment_Pratos.OnFragmentInteractionListener,
+        Fragment_Cardapio.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act__detalhe__estabelecimento);
+        //#############################################
+        //####Botões que iram chamar os Fragments######
+        //#############################################
+        ImageButton btn_Ambiente = findViewById(R.id.btn_Ambiente);
+        ImageButton btn_Pratos = findViewById(R.id.btn_Pratos);
+        ImageButton btn_Cardapio = findViewById(R.id.btn_Cardapio);
+        btn_Ambiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Fragment_Ambiente();
+                getSupportFragmentManager() .beginTransaction() .replace(R.id.parafragmet, fragment) .commit();
+            }
+        });
+
+        btn_Pratos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Fragment_Pratos();
+                getSupportFragmentManager() .beginTransaction() .replace(R.id.parafragmet, fragment) .commit();
+            }
+        });
+
+        btn_Cardapio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Fragment_Cardapio();
+                getSupportFragmentManager() .beginTransaction() .replace(R.id.parafragmet, fragment) .commit();
+            }
+        });
+
+        //##########################################
+        //##########################################
+        //##########################################
 
         Intent it = getIntent();
-        String parametro = (String) it.getSerializableExtra("nome");
+        String listNome = (String) it.getSerializableExtra("Nome");
+        String listCidade = (String) it.getSerializableExtra("Cidade");
+        ImageView listImagem = (ImageView) it.getSerializableExtra("Imagem");
+        String listEndereco = (String) it.getSerializableExtra("Endereço");
+        String identificador = (String) it.getSerializableExtra("Identificador");
+        Integer id_list = (Integer) it.getSerializableExtra("Id_List");
 
-        TextView txt = (TextView) findViewById(R.id.txt_parametro);
-        txt.setText(parametro);
+        TextView txt_Nome = findViewById(R.id.txt_Nome);
+        TextView txt_Comentario = findViewById(R.id.txt_Comentario);
+        TextView txt_Tipo = findViewById(R.id.txt_Tipo);
+        TextView txt_Status = findViewById(R.id.txt_Status);
+        TextView txt_Horario = findViewById(R.id.txt_Horario);
+        TextView txt_Endereco = findViewById(R.id.txt_Endereco);
+        TextView txt_Telefone = findViewById(R.id.txt_Telefone);
+        TextView txt_Site = findViewById(R.id.txt_Site);
+        ImageView img = findViewById(R.id.img);
+
+        txt_Nome.setText(listNome);
+
+        if (identificador.equals("1")) {
+            txt_Tipo.setText("Restaurante");
+            txt_Status.setText("Aberto");
+            txt_Horario.setText("Horário:  11:00–00:00");
+
+            if (id_list == 0 || id_list == 6) {
+                String comentario = (String) "Restaurante e bar serve menu de especialidades regionais," +
+                        " em casa animada com salão aberto para a calçada.";
+                txt_Comentario.setText(comentario);
+                img.setImageResource(R.drawable.r_1);
+                txt_Endereco.setText(listEndereco + listCidade);
+                txt_Telefone.setText("(83) 3322-4232");
+                txt_Site.setText("http://www.bardocuscuz.com/");
+            }else if (id_list == 1 || id_list == 7){
+                String comentario = (String) "Bacalhau, paella e carnes assadas em espaço com decoração contemporânea," +
+                        " área infantil e clima descontraído.";
+                txt_Comentario.setText(comentario);
+                img.setImageResource(R.drawable.r_2);
+                txt_Endereco.setText(listEndereco + listCidade);
+                txt_Telefone.setText("(83) 3341-6464");
+                txt_Site.setText("http://campinagrill.com.br");
+            }else if (id_list == 2 || id_list == 8){
+                String comentario = (String) "Restaurante espaçoso serve menu de especialidades nordestinas diversas " +
+                        "em ambiente familiar rústico-elegante.";
+                txt_Comentario.setText(comentario);
+                img.setImageResource(R.drawable.r_3);
+                txt_Endereco.setText(listEndereco + listCidade);
+                txt_Telefone.setText("(83) 3321-2877");
+                txt_Site.setText("https://pt-br.facebook.com/pages/Manoel-da-Carne-de-Sol");
+
+            }else if(id_list == 3 || id_list == 9){
+                String comentario = (String) "O menu chinês fumegante inventivo e tradicional à la carte, em espaço despretensioso," +
+                        " aberto e ventilado.";
+                txt_Comentario.setText(comentario);
+                img.setImageResource(R.drawable.r_4);
+                txt_Endereco.setText(listEndereco + listCidade);
+                txt_Telefone.setText("(83) 3341-1812");
+                txt_Site.setText("http://www.restaurantechinataiwan.com.br/");
+
+            }else if (id_list == 4 || id_list == 10){
+                String comentario = (String) "O menu chinês fumegante inventivo e tradicional à la carte, em espaço despretensioso," +
+                        " aberto e ventilado.";
+                txt_Comentario.setText(comentario);
+                img.setImageResource(R.drawable.r_5);
+                txt_Endereco.setText(listEndereco + listCidade);
+                txt_Telefone.setText("(83) 3341-1812");
+                txt_Site.setText("http://www.restaurantechinataiwan.com.br/");
+
+            }
+
+
+
+        } else{
+            Toast.makeText(getApplicationContext(), "afafaf", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
--- app/src/main/java/com/example/tecinfo/apk_final_tecinfo/Act_Lista_De_Locais.java
+++ app/src/main/java/com/example/tecinfo/apk_final_tecinfo/Act_Lista_De_Locais.java
@@ -22,7 +22,7 @@ public class Act_Lista_De_Locais extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intentRecebido = getIntent();
-        String identificador =  intentRecebido.getStringExtra("identificador");
+        final String identificador =  intentRecebido.getStringExtra("identificador");
         getSupportActionBar().setTitle("");
         getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.color.Cor_Icones_NavDrawer));
 
@@ -42,9 +42,16 @@ public class Act_Lista_De_Locais extends AppCompatActivity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Intent it = new Intent(getApplicationContext(), Act_Detalhe_Estabelecimento.class);
-                    it.putExtra("nome", listViews.get(position).getNome());
+                    it.putExtra("Nome", listViews.get(position).getNome());
+                    it.putExtra("Imagem", listViews.get(position).getImagen());
+                    it.putExtra("Endereço", listViews.get(position).getEndereco());
+                    it.putExtra("Cidade", listViews.get(position).getCidade());
+                    it.putExtra("Identificador",  identificador);
+                    it.putExtra("Id_List", position);
                     startActivity(it);
                     finish();
+                    //Toast.makeText(getApplicationContext(), "gjaçjga" +position + identificador, Toast.LENGTH_LONG).show();
+
                 }
             });
 
@@ -52,38 +59,119 @@ public class Act_Lista_De_Locais extends AppCompatActivity {
             getSupportActionBar().setTitle("Pizzarias");
 
             android.widget.ListView lista = (android.widget.ListView) findViewById(R.id.lv_Locais);
-            ArrayAdapter adapter = new ListViewAdapter(this, adicionarLocais(identificador));
+            final ArrayList <ListView> listViews = adicionarLocais(identificador);
+            ArrayAdapter adapter = new ListViewAdapter(this, listViews);
             lista.setAdapter(adapter);
+
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent it = new Intent(getApplicationContext(), Act_Detalhe_Estabelecimento.class);
+                    it.putExtra("Nome", listViews.get(position).getNome());
+                    it.putExtra("Imagem", listViews.get(position).getImagen());
+                    it.putExtra("Endereço", listViews.get(position).getEndereco());
+                    it.putExtra("Cidade", listViews.get(position).getCidade());
+                    it.putExtra("Identificador",  identificador);
+                    it.putExtra("Id_List", position);
+                    startActivity(it);
+                    finish();
+                }
+            });
+
 
         }else if (identificador.equals("3")){
             getSupportActionBar().setTitle("Parques");
 
             android.widget.ListView lista = (android.widget.ListView) findViewById(R.id.lv_Locais);
-            ArrayAdapter adapter = new ListViewAdapter(this, adicionarLocais(identificador));
+            final ArrayList <ListView> listViews = adicionarLocais(identificador);
+            ArrayAdapter adapter = new ListViewAdapter(this, listViews);
             lista.setAdapter(adapter);
+
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent it = new Intent(getApplicationContext(), Act_Detalhe_Estabelecimento.class);
+                    it.putExtra("Nome", listViews.get(position).getNome());
+                    it.putExtra("Imagem", listViews.get(position).getImagen());
+                    it.putExtra("Endereço", listViews.get(position).getEndereco());
+                    it.putExtra("Cidade", listViews.get(position).getCidade());
+                    it.putExtra("Identificador",  identificador);
+                    it.putExtra("Id_List", position);
+                    startActivity(it);
+                    finish();
+                }
+            });
 
         }else if (identificador.equals("4")){
             getSupportActionBar().setTitle("Piscinas");
 
             android.widget.ListView lista = (android.widget.ListView) findViewById(R.id.lv_Locais);
-            ArrayAdapter adapter = new ListViewAdapter(this, adicionarLocais(identificador));
+            final ArrayList <ListView> listViews = adicionarLocais(identificador);
+            ArrayAdapter adapter = new ListViewAdapter(this, listViews);
             lista.setAdapter(adapter);
+
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent it = new Intent(getApplicationContext(), Act_Detalhe_Estabelecimento.class);
+                    it.putExtra("Nome", listViews.get(position).getNome());
+                    it.putExtra("Imagem", listViews.get(position).getImagen());
+                    it.putExtra("Endereço", listViews.get(position).getEndereco());
+                    it.putExtra("Cidade", listViews.get(position).getCidade());
+                    it.putExtra("Identificador",  identificador);
+                    it.putExtra("Id_List", position);
+                    startActivity(it);
+                    finish();
+                }
+            });
+
         }else if (identificador.equals("5")){
 
             getSupportActionBar().setTitle("Lanchonetes");
 
             android.widget.ListView lista = (android.widget.ListView) findViewById(R.id.lv_Locais);
-            ArrayAdapter adapter = new ListViewAdapter(this, adicionarLocais(identificador));
+            final ArrayList <ListView> listViews = adicionarLocais(identificador);
+            ArrayAdapter adapter = new ListViewAdapter(this, listViews);
             lista.setAdapter(adapter);
 
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent it = new Intent(getApplicationContext(), Act_Detalhe_Estabelecimento.class);
+                    it.putExtra("Nome", listViews.get(position).getNome());
+                    it.putExtra("Imagem", listViews.get(position).getImagen());
+                    it.putExtra("Endereço", listViews.get(position).getEndereco());
+                    it.putExtra("Cidade", listViews.get(position).getCidade());
+                    it.putExtra("Identificador",  identificador);
+                    it.putExtra("Id_List", position);
+                    startActivity(it);
+                    finish();
+                }
+            });
 
         }else if (identificador.equals("6")){
 
             getSupportActionBar().setTitle("Outros");
 
             android.widget.ListView lista = (android.widget.ListView) findViewById(R.id.lv_Locais);
-            ArrayAdapter adapter = new ListViewAdapter(this, adicionarLocais(identificador));
+            final ArrayList <ListView> listViews = adicionarLocais(identificador);
+            ArrayAdapter adapter = new ListViewAdapter(this, listViews);
             lista.setAdapter(adapter);
+
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent it = new Intent(getApplicationContext(), Act_Detalhe_Estabelecimento.class);
+                    it.putExtra("Nome", listViews.get(position).getNome());
+                    it.putExtra("Imagem", listViews.get(position).getImagen());
+                    it.putExtra("Endereço", listViews.get(position).getEndereco());
+                    it.putExtra("Cidade", listViews.get(position).getCidade());
+                    it.putExtra("Identificador",  identificador);
+                    startActivity(it);
+                    finish();
+
+                }
+            });
 
 
         }
@@ -96,48 +184,97 @@ public class Act_Lista_De_Locais extends AppCompatActivity {
 
 
         if (id.equals("1")) {
-            ListView e = new ListView("Bar do Cuscuz",
-                    "R. Dr. Severino Cruz, 771 - Centro", "Campina Grande - PB", R.drawable.r_1, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
-            list.add(e);
-            e = new ListView("Campina Grill Restaurante",
-                    "Rua Manoel Tavares, 1900 - Jardim Tavares", "Campina Grande - PB", R.drawable.r_2, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
-            list.add(e);
-            e = new ListView("Manoel da Carne de Sol",
-                    "R. Félix Araújo, 263 - Centro", "Campina Grande - PB", R.drawable.r_3, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
-            list.add(e);
 
-            e = new ListView("Restaurante China Taiwan Campina Grande",
-                    "R. Silva Jardim, 150 - Jardim Tavares", "Campina Grande - PB", R.drawable.r_4, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
-            list.add(e);
+             int x = 0;
+             while (x<5){
+                 x++;
 
-            e = new ListView("Frida Tacos",
-                    "R. Odon Bezerra, 392 - Liberdade", "Campina Grande - PB", R.drawable.r_5, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
-            list.add(e);
+                 ListView e = new ListView("Bar do Cuscuz",
+                         "R. Dr. Severino Cruz, 771 - Centro", "Campina Grande - PB", R.drawable.r_1, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                 list.add(e);
+                 e = new ListView("Campina Grill Restaurante",
+                         "Rua Manoel Tavares, 1900 - Jardim Tavares", "Campina Grande - PB", R.drawable.r_2, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                 list.add(e);
+                 e = new ListView("Manoel da Carne de Sol",
+                         "R. Félix Araújo, 263 - Centro", "Campina Grande - PB", R.drawable.r_3, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                 list.add(e);
+
+                 e = new ListView("Restaurante China Taiwan Campina Grande",
+                         "R. Silva Jardim, 150 - Jardim Tavares", "Campina Grande - PB", R.drawable.r_4, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                 list.add(e);
+
+                 e = new ListView("Frida Tacos",
+                         "R. Odon Bezerra, 392 - Liberdade", "Campina Grande - PB", R.drawable.r_5, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                 list.add(e);
+
+             }
 
         }else if(id.equals("2")){
+            int x = 0;
+
+            while (x<5){
+                x++;
+
+                ListView e = new ListView("Domino's Pizza",
+                        "R. João Maria de Souza Ribeiro, 64 - Jardim Tavares", "Campina Grande - PB", R.drawable.p_1, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
+                e = new ListView("Pizza Hut",
+                        "Av. Pref. Severino Bezerra Cabral, 1050 - Loja 3005 - Catolé", "Campina Grande - PB", R.drawable.p_2, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
+                e = new ListView("Pizzaria do Beguinha",
+                        "Rua Manoel Tavares, 1142 - Lauritzen", "Campina Grande - PB", R.drawable.p_3, R.drawable.star1,R.drawable.p_4, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
+                e = new ListView("Pizzaria Express",
+                        "R. Clementino Siqueira, 295 - Jardim Tavares", "Campina Grande - PB", R.drawable.p_4, R.drawable.p_4, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
 
 
-
+            }
 
         }else if(id.equals("3")){
+            int x = 0;
 
+            while (x<5){
+                x++;
 
-
+            }
 
         }else if(id.equals("4")){
+            int x = 0;
 
+            while (x<5){
+                x++;
 
+                ListView e = new ListView("Afrafep - Balneário",
+                        " R. Vicente Gomes Almeida, 58108-293 - Bodocongó", "Campina Grande - PB", R.drawable.pisc_1, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
+                e = new ListView("Balneário Roda D'água",
+                        "R. Sn 107 - Aeroporto, Bayeux - PB", "Bayenux - PB", R.drawable.pisc_2, R.drawable.star1, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
+                e = new ListView("Piscina Paraiso Do Castanho",
+                        "R. João Miranda, 1417 - Castanho", "Queimadas - PB",  R.drawable.pisc_3, R.drawable.star1, R.drawable.p_4, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
+                e = new ListView("Piscinas Fiber ®",
+                        "Av. Pref. Severino Bezerra Cabral, 155 - José Pinheiro", "Campina Grande - PB",  R.drawable.pisc_4, R.drawable.p_4, R.drawable.star1, R.drawable.star1, R.drawable.star2, R.drawable.star2);
+                list.add(e);
 
+            }
 
         }else if(id.equals("5")){
+            int x = 0;
 
+            while (x<5){
+                x++;
 
-
+            }
 
         }else if(id.equals("6")){
+            int x = 0;
 
+            while (x<5){
+                x++;
 
-
+            }
 
         }
 
